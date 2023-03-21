d9978c75e078b7ec7cce76bfdc4772846f55494a
Michael Plantamura <105463635+Michaelplant2@users.noreply.github.com>
2023-03-21 16:35:16 -0400

    laid out interface

---
+++ script.js
@@ -0,0 +1,18 @@
+let firstCard = 11;
+let secondCard= 11;
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+
+function startGame() {
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "You have Blackjack!";
+    hasBlackJack = true;
+  } else {
+    message = "You bust";
+    isAlive = false;
+  }
+}
