0bcf94adef7a9ceb6e69a7eacf7f22e8e198c64f
Michael Plantamura <105463635+Michaelplant2@users.noreply.github.com>
2023-03-21 16:52:40 -0400

    JavaScript added

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 let firstCard = 11;
-let secondCard= 11;
+let secondCard= 10;
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
 
 function startGame() {
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
+    sumEl.textContent = "Sum: " + sum;
   if (sum <= 20) {
     message = "Do you want to draw a new card?";
   } else if (sum === 21) {
@@ -15,4 +20,5 @@ function startGame() {
     message = "You bust";
     isAlive = false;
   }
+  messageEl.textContent = message;
 }
